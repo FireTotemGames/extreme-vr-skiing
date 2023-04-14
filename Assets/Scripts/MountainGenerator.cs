97f39e1f0efba012bcbbf49b98baf8fd46bc70d1
Noel Treese <noeltreese@gmail.com>
2023-04-14 02:32:24 +0200

    Fix terrain not generating from 0

--- Assets/Scripts/MountainGenerator.cs
+++ Assets/Scripts/MountainGenerator.cs
@@ -9,6 +9,7 @@ public class MountainGenerator : MonoBehaviour
     /* ======================================================================================================================== */
     [SerializeField] int width = 100;
     [SerializeField] int height = 100;
+    [SerializeField] int treeAmount = 1000;
     [SerializeField] float scale = 20.0f;
     [SerializeField] float heightMultiplier = 5.0f;
     [SerializeField] int tilesX = 2;
@@ -58,15 +59,15 @@ public class MountainGenerator : MonoBehaviour
 
         for (int z = 0; z <= height; z++)
         {
-            for (int x = 0; x < width; x++)
+            for (int x = -width/2; x < width/2; x++)
             {
                 float heightValue = Mathf.PerlinNoise((float)(width + x) / (width) * scale, (float)(tilesZ * height + z) / (height) * scale) * heightMultiplier;
 
-                vertices[z * width + x] = new Vector3(x, heightValue, z);
+                vertices[z * width + (x + width/2)] = new Vector3(x, heightValue, z);
 
-                if (x < width - 1 && z < height)
+                if ((x + width/2) < width - 1 && z < height)
                 {
-                    int vertexIndex = z * width + x;
+                    int vertexIndex = z * width + (x + width/2);
                     triangles[triangleIndex + 0] = vertexIndex;
                     triangles[triangleIndex + 1] = vertexIndex + width;
                     triangles[triangleIndex + 2] = vertexIndex + width + 1;
@@ -102,7 +103,7 @@ public class MountainGenerator : MonoBehaviour
     public void AddTile()
     {
         GameObject tile = new GameObject("Tile_" + 0f + "_" + tilesZ);
-        tile.transform.position = new Vector3(width, 0, tilesZ * height);
+        tile.transform.position = new Vector3(0, 0, tilesZ * height);
         //tile.transform.rotation = Quaternion.Euler(0, 0, 180);
         tile.transform.parent = transform;
 
@@ -136,10 +137,10 @@ public class MountainGenerator : MonoBehaviour
 
     private void SpawnTrees()
     {
-        for (int i = 0; i < 1000; i++)
+        for (int i = 0; i < treeAmount; i++)
         {
             Vector3 randomPosition = new Vector3();
-            randomPosition.x = width + Random.Range(0, width);
+            randomPosition.x = Random.Range(-width/2, width/2);
             randomPosition.z = tilesZ * height + Random.Range(0, height);
             randomPosition.y = Mathf.PerlinNoise(randomPosition.x / scale, randomPosition.z / scale) * heightMultiplier;
             Instantiate(treePrefab, randomPosition, Quaternion.identity, meshFilters.Last().transform);
