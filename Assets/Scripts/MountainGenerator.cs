53710af229fc97b191375771a8f13fc5e83f2ade
Noel Treese <noeltreese@gmail.com>
2023-04-14 11:01:42 +0200

    Terrain Test

--- Assets/Scripts/MountainGenerator.cs
+++ Assets/Scripts/MountainGenerator.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class MountainGenerator : MonoBehaviour
 {
@@ -25,7 +28,6 @@ public class MountainGenerator : MonoBehaviour
 
     public int gaussianOffset = 3;
     public int gaussianWidth = 10;
-    public float edgeRadius = 40f;
     /* ======================================================================================================================== */
     /* UNITY CALLBACKS                                                                                                          */
     /* ======================================================================================================================== */
@@ -139,19 +141,141 @@ public class MountainGenerator : MonoBehaviour
         
     }
 
+    // private void SpawnTrees()
+    // {
+    //     for (int i = 0; i < treeAmount; i++)
+    //     {
+    //         Vector3 randomPosition = new Vector3();
+    //         randomPosition.x = Random.Range(-width/2, width/2);
+    //         randomPosition.z = tilesZ * height + Random.Range(0, height);
+    //         randomPosition.y = Mathf.PerlinNoise(randomPosition.x / scale, randomPosition.z / scale) * heightMultiplier;
+    //         Instantiate(treePrefab, randomPosition, Quaternion.identity, meshFilters.Last().transform);
+    //     }
+    //     
+    // }
+    
     private void SpawnTrees()
     {
+        
         for (int i = 0; i < treeAmount; i++)
         {
             Vector3 randomPosition = new Vector3();
             randomPosition.x = Random.Range(-width/2, width/2);
             randomPosition.z = tilesZ * height + Random.Range(0, height);
-            randomPosition.y = Mathf.PerlinNoise(randomPosition.x / scale, randomPosition.z / scale) * heightMultiplier;
+
+            // Calculate distance from center normalized to [-1, 1]
+            float normalizedDistanceFromCenter = Mathf.Sqrt(randomPosition.x * randomPosition.x) / (width / 2f) * 2f - 1f;
+
+            // Calculate Gaussian distribution weight based on normalized distance from center
+            float gaussianWeight = Mathf.Exp(-0.5f * normalizedDistanceFromCenter * normalizedDistanceFromCenter / (gaussianWidth * gaussianWidth));
+
+            randomPosition.x = NextGaussian() * width;
+            // Calculate Perlin noise value
+            float perlinNoise = Mathf.PerlinNoise(randomPosition.x / scale, randomPosition.z / scale) * heightMultiplier;
+
+            // Calculate final y position as a weighted average of Perlin noise and Gaussian weight
+            //randomPosition.x = gaussianWeight;
+            randomPosition.y = perlinNoise;
+
+            
+
             Instantiate(treePrefab, randomPosition, Quaternion.identity, meshFilters.Last().transform);
         }
-        
     }
-
+    
+    public static float NextGaussian() {
+        float v1, v2, s;
+        do {
+            v1 = 2.0f * Random.Range(0f,1f) - 1.0f;
+            v2 = 2.0f * Random.Range(0f,1f) - 1.0f;
+            s = v1 * v1 + v2 * v2;
+        } while (s >= 1.0f || s == 0f);
+        s = Mathf.Sqrt((-2.0f * Mathf.Log(s)) / s);
+ 
+        return v1 * s;
+    }
+    
+    // private void SpawnTrees()
+    // {
+    //     List<GameObject> trees = new List<GameObject>();
+    //     float treeSpacing = 1f;
+    //     float[,] distribution = new float[width, width];
+    //     float sum = 0f;
+    //
+    //     // Calculate Gaussian distribution
+    //     for (int x = 0; x < width; x++)
+    //     {
+    //         for (int z = 0; z < width; z++)
+    //         {
+    //             float exponent = -((x - mean) * (x - mean) + (z - mean) * (z - mean)) / (2f * standardDeviation * standardDeviation);
+    //             distribution[x, z] = Mathf.Exp(exponent);
+    //             sum += distribution[x, z];
+    //         }
+    //     }
+    //
+    //     // Normalize distribution
+    //     for (int x = 0; x < width; x++)
+    //     {
+    //         for (int z = 0; z < width; z++)
+    //         {
+    //             distribution[x, z] /= sum;
+    //         }
+    //     }
+    //
+    //     // Spawn trees with Gaussian distribution
+    //     for (int i = 0; i < treeAmount; i++)
+    //     {
+    //         // Choose a random position based on the Gaussian distribution
+    //         Vector2 randomPosition = RandomGaussianPosition(distribution);
+    //         Vector3 spawnPosition = new Vector3(randomPosition.x - width / 2f, 0f, tilesZ * height + randomPosition.y);
+    //         spawnPosition.y = Mathf.PerlinNoise(spawnPosition.x, spawnPosition.z) * heightMultiplier;
+    //
+    //         // Check if position is already occupied by a tree
+    //         bool positionIsOccupied = false;
+    //         foreach (GameObject tree in trees)
+    //         {
+    //             if (Vector3.Distance(tree.transform.position, spawnPosition) < treeSpacing)
+    //             {
+    //                 positionIsOccupied = true;
+    //                 break;
+    //             }
+    //         }
+    //
+    //         // If position is occupied, skip this iteration of the loop
+    //         if (positionIsOccupied)
+    //         {
+    //             continue;
+    //         }
+    //
+    //         // Spawn tree at position
+    //         GameObject treeObject = Instantiate(treePrefab, spawnPosition, Quaternion.identity, meshFilters.Last().transform);
+    //         trees.Add(treeObject);
+    //     }
+    // }
+    //
+    // private Vector2 RandomGaussianPosition(float[,] distribution)
+    // {
+    //     float randomX = Random.Range(0f, 1f);
+    //     float randomY = Random.Range(0f, 1f);
+    //     float sum = 0f;
+    //
+    //     // Find position in distribution that corresponds to the random values
+    //     for (int x = 0; x < width; x++)
+    //     {
+    //         for (int z = 0; z < width; z++)
+    //         {
+    //             sum += distribution[x, z];
+    //             if (randomX <= sum && randomY <= distribution[x, z])
+    //             {
+    //                 return new Vector2(x, z);
+    //             }
+    //         }
+    //     }
+    //
+    //     // If no position was found, return a random position
+    //     return new Vector2(Random.Range(0, width), Random.Range(0, width));
+    // }
+    
     /* ======================================================================================================================== */
     /* EVENT CALLERS                                                                                                            */
     /* ======================================================================================================================== */
