c918a78da41634fad7cf73488bc3befb7e6ac149
Sebastian Uitz <firetotemgames@gmail.com>
2023-04-15 13:32:46 +0200

    Implement danger sound

--- Assets/Avalanche.cs
+++ Assets/Avalanche.cs
@@ -1,8 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
+using FMOD.Studio;
 using FMODUnity;
 using UnityEngine;
-using UnityEngine.Serialization;
+using STOP_MODE = FMOD.Studio.STOP_MODE;
 
 public class Avalanche : MonoBehaviour
 {
@@ -12,6 +11,12 @@ public class Avalanche : MonoBehaviour
 
     [SerializeField] private GameObject deathTrigger;
     [SerializeField] private StudioEventEmitter avalancheSound;
+    [SerializeField] private Transform player;
+    [SerializeField] private float dangerSoundStartDistance;
+    [SerializeField] private float dangerSoundStopDistance;
+    
+    private EventInstance dangerSound;
+    private bool avalancheActive;
 
     /* ======================================================================================================================== */
     /* UNITY CALLBACKS                                                                                                          */
@@ -20,6 +25,38 @@ public class Avalanche : MonoBehaviour
     private void Start()
     {
         deathTrigger.SetActive(false);
+        dangerSound = RuntimeManager.CreateInstance("event:/sounds/danger");
+    }
+
+    private void Update()
+    {
+        if (avalancheActive == false)
+        {
+            return;
+        }
+
+        float distance = Mathf.Abs(transform.position.z - player.position.z);
+        if (distance < dangerSoundStartDistance)
+        {
+            dangerSound.getPlaybackState(out PLAYBACK_STATE state);
+            if (state != PLAYBACK_STATE.PLAYING)
+            {
+                Debug.Log("start danger sound");
+                dangerSound.start();
+            }
+
+            float speed = 1f - Mathf.Clamp01(distance / dangerSoundStartDistance);
+            dangerSound.setParameterByName("speed", speed);
+        }
+        else if (distance > dangerSoundStopDistance)
+        {
+            dangerSound.getPlaybackState(out PLAYBACK_STATE state);
+            if (state == PLAYBACK_STATE.PLAYING)
+            {
+                Debug.Log("stop danger sound");
+                dangerSound.stop(STOP_MODE.ALLOWFADEOUT);
+            }
+        }
     }
 
     /* ======================================================================================================================== */
@@ -36,10 +73,11 @@ public class Avalanche : MonoBehaviour
 
     public void ActivateDeathTrigger()
     {
+        avalancheActive = true;
         deathTrigger.SetActive(true);
         avalancheSound.Play();
     }
-    
+
     /* ======================================================================================================================== */
     /* EVENT CALLERS                                                                                                            */
     /* ======================================================================================================================== */
